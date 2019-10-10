7863c27bbd6c1043fb09b1f7dc5bd036e4ca1200
wangdong <597365277@qq.com>
2019-10-10 11:11:22 +0800

    添加项目文件。

---
+++ TestSwagger.Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSwagger.Model
+{
+    /// <summary>
+    /// 实体-用户类
+    /// </summary>
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ TestSwagger.WebApi/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TestSwagger.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "API",
+                    Description = "api文档",
+                    TermsOfService = "None"
+                });
+                var basePath = AppContext.BaseDirectory;
+                var xmlPath = Path.Combine(basePath, "TestSwagger.WebApi.xml");
+                var xmlPathByModel = Path.Combine(basePath, "TestSwagger.Model.xml");
+                options.IncludeXmlComments(xmlPathByModel);
+                //true表示生成控制器描述，包含true的IncludeXmlComments重载应放在最后，或者两句都使用true
+                options.IncludeXmlComments(xmlPath, true);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseStaticFiles();//启用默认文件夹wwwroot
+            app.UseSwagger();
+            app.UseSwaggerUI(action =>
+            {
+                action.ShowExtensions();
+                action.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+            });
+        }
+    }
+}
